00fa8d0f255fa13ac8f14a44dc7a75c35ee5151c
saintlySatanic <ujjwalbrahmin@gmail.com>
2025-05-28 11:39:57 +0530

    chore: hello world test new setup

---
+++ fundamentals/HelloWorld.java
@@ -0,0 +1,8 @@
+package fundamentals;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print the message to the console
+        System.out.println("Hello, World!");
+    }
+}
