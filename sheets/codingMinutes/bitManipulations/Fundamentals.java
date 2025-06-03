ea1338a936b8c1ff2310ee2682cb9233681cec77
saintlySatanic <ujjwalbrahmin@gmail.com>
2025-06-03 08:54:35 +0530

    feat: 2 power multiply

--- sheets/codingMinutes/bitManipulations/Fundamentals.java
+++ sheets/codingMinutes/bitManipulations/Fundamentals.java
@@ -12,12 +12,18 @@ public class Fundamentals {
         return (n & 1) == 0 ? false: true;
     }
 
+    static int multiplyBy2Power(int n, int pow) {
+        // keep in mind the overflow condition, which will be based on the memory architecture / dtype which you choose (32 bit, 64 bit, etc)
+        return n<<pow;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int pos = sc.nextInt();
 
         System.out.printf("Is %s pos bit set: %s \n", pos, isIthBitSet(n, pos));
-        System.out.printf("Is Odd: %s ", isOdd(n));
+        System.out.printf("Is Odd: %s \n", isOdd(n));
+        System.out.printf("%s times 2 power %s is %s \n", n, pos, multiplyBy2Power(n, pos));
     }
 }
