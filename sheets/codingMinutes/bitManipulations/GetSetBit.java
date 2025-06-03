63cffe901dacb13353c3d16f75b5203f9563f9c7
saintlySatanic <ujjwalbrahmin@gmail.com>
2025-06-03 10:12:23 +0530

    feat: clear last i bits

---
+++ sheets/codingMinutes/bitManipulations/GetSetBit.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class GetSetBit {
+    static int getItBit(int n, int i) {
+        return (n&(1<<i)) !=0 ? 1:0;
+    }
+
+    static int setIthBit(int n, int i) {
+        return n | (1<<i);
+    }
+
+    static int clearIthBit(int n, int i) {
+        /*
+        You need to create a mask where all bits are set except bit at i
+        */
+       int mask = ~(1<<i);
+        return n & mask;
+    }
+
+    static int updateIthBit(int n, int i, int value){
+        /*
+        Update n at position i based on the input value(0,1)
+        */
+       int nWithClearedIthBit = clearIthBit(n, i);
+       int mask = (value<<i);
+       // if value is 0 should not matter, if 1 then 1 will be present in the exact position where we want it to be
+
+       return nWithClearedIthBit | mask;
+    }
+
+    static int clearLastIBits(int n, int i){
+        return (n>>i)<<i; // Solution 1
+
+        // int mask = -1<<i;
+        // return n & mask;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int i = sc.nextInt();
+        System.out.printf("Last %s bits cleared of %s is %s", i, n, clearLastIBits(n, i));
+    }
+}
