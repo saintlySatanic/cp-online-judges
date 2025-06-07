06e232132e5a84f5a307f260ce8bb651cf735181
saintlySatanic <ujjwalbrahmin@gmail.com>
2025-06-07 10:40:03 +0530

    feat: power of 2

--- sheets/codingMinutes/bitManipulations/PowerOf2.java
+++ sheets/codingMinutes/bitManipulations/PowerOf2.java
@@ -18,10 +18,18 @@ public class PowerOf2 {
         return true;
     }
 
+    static boolean isPowerOf2Optimized(int n) {
+        /*
+        If N is a power of 2 then N-1 bits will all be set
+        */
+       return (n&(n-1)) == 0;
+    }
+
     public static void main(String[] agrs) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         System.out.printf("Is %s power of 2 %s \n", n, isPowerOf2(n));
+        System.out.printf("Optimzed : Is %s power of 2 %s \n", n, isPowerOf2Optimized(n));
     }
 }
