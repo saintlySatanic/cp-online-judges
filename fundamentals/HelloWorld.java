c7131b2c239c6bae2dde04cc4e0e9ad9a985d74e
saintlySatanic <ujjwalbrahmin@gmail.com>
2025-06-07 13:08:32 +0530

    HOTFIX: deleted fundamentals dir

--- fundamentals/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package fundamentals;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Print the message to the console
-        System.out.println("Hello, World!");
-    }
-}
