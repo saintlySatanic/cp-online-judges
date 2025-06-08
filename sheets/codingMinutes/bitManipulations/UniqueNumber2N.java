c84b87ed32a130a35c1e0492fa4e54d70dd08057
saintlySatanic <ujjwalbrahmin@gmail.com>
2025-06-08 06:22:43 +0530

    feat: unique number

---
+++ sheets/codingMinutes/bitManipulations/UniqueNumber2N.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UniqueNumber2N {
+    static int findUniques(int[] arr) {
+        int unique = 0;
+        for(int i=0;i<arr.length;i++) {
+            unique = unique^arr[i];
+        }
+
+        return unique;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] inputs = new int[n];
+        for(int i=0;i<n;i++) inputs[i] = sc.nextInt();
+
+        System.err.printf("Unique number is : %s", findUniques(inputs));
+    }
+}
