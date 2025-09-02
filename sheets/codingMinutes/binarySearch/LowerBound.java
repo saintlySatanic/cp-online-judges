014c594376cc4b4934e3c0e04826b043c937c22a
saintlySatanic <ujjwalbrahmin@gmail.com>
2025-09-02 19:57:05 +0530

    feat: upper and lower bound

---
+++ sheets/codingMinutes/binarySearch/LowerBound.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class LowerBound {
+    public static int lowerBound(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        int ans = nums.length;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                end = mid-1;
+                ans = mid;
+            } else if (nums[mid] > target) {
+                end = mid -1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int upperBound(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        int ans = nums.length;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                start = mid + 1;
+                ans = mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int[] nums = new int[x];
+        for(int i= 0; i<x; i++) nums[i] = sc.nextInt();
+
+        int target = sc.nextInt();
+        int lowerBound = lowerBound(nums, target);
+
+        System.out.println("Lower Bound is : " + lowerBound);
+
+        int upperBound = upperBound(nums, target);
+        System.out.println("Upper Bound is : " + upperBound);
+    }
+}
